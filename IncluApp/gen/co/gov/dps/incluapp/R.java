1edffe7c89060aa76949a353205588fe5cde5823
oscar david gallon rosero <oscarg798@gmail.com>
2014-12-04 17:33:03 -0500

    merge bugs fixed

--- IncluApp/gen/co/gov/dps/incluapp/R.java
+++ IncluApp/gen/co/gov/dps/incluapp/R.java
@@ -51,28 +51,37 @@ public final class R {
         public static final int task=0x7f02000a;
     }
     public static final class id {
-<<<<<<< HEAD
-        public static final int action_example=0x7f0a0008;
-        public static final int action_settings=0x7f0a0007;
-        public static final int container=0x7f0a0001;
+        public static final int LinearLayout1=0x7f0a000a;
+        public static final int action_example=0x7f0a000e;
+        public static final int action_settings=0x7f0a000d;
+        public static final int container=0x7f0a0000;
         public static final int counter=0x7f0a0005;
-        public static final int drawer_layout=0x7f0a0000;
+        public static final int drawer_layout=0x7f0a0001;
         public static final int icon=0x7f0a0003;
+        public static final int im_icon=0x7f0a0006;
+        public static final int im_profile=0x7f0a0009;
+        public static final int lv_expiriences=0x7f0a000b;
         public static final int navigation_drawer=0x7f0a0002;
-        public static final int section_label=0x7f0a0006;
+        public static final int section_label=0x7f0a000c;
         public static final int title=0x7f0a0004;
+        public static final int txt_description=0x7f0a0008;
+        public static final int txt_title=0x7f0a0007;
     }
     public static final class layout {
-        public static final int activity_navigation_main=0x7f030000;
-        public static final int activity_timeline=0x7f030001;
-        public static final int drawer_list_item=0x7f030002;
-        public static final int fragment_navigation_activity_main=0x7f030003;
-        public static final int fragment_navigation_drawer=0x7f030004;
+        public static final int activity_experiencias=0x7f030000;
+        public static final int activity_navigation_main=0x7f030001;
+        public static final int activity_timeline=0x7f030002;
+        public static final int drawer_list_item=0x7f030003;
+        public static final int experiencias_custom_list_layout=0x7f030004;
+        public static final int fragment_experiencias=0x7f030005;
+        public static final int fragment_navigation_activity_main=0x7f030006;
+        public static final int fragment_navigation_drawer=0x7f030007;
     }
     public static final class menu {
-        public static final int global=0x7f090000;
-        public static final int navigation_activity_main=0x7f090001;
-        public static final int timeline=0x7f090002;
+        public static final int experiencias=0x7f090000;
+        public static final int global=0x7f090001;
+        public static final int navigation_activity_main=0x7f090002;
+        public static final int timeline=0x7f090003;
     }
     public static final class string {
         public static final int action_example=0x7f06000a;
@@ -82,37 +91,12 @@ public final class R {
         public static final int hello_world=0x7f060001;
         public static final int navigation_drawer_close=0x7f060009;
         public static final int navigation_drawer_open=0x7f060008;
+        public static final int title_activity_experiencias=0x7f06000c;
         public static final int title_activity_navigation_activity_main=0x7f060003;
         public static final int title_section1=0x7f060004;
         public static final int title_section2=0x7f060005;
         public static final int title_section3=0x7f060006;
         public static final int title_section4=0x7f060007;
-=======
-        public static final int LinearLayout1=0x7f080005;
-        public static final int action_settings=0x7f080007;
-        public static final int container=0x7f080000;
-        public static final int im_icon=0x7f080001;
-        public static final int im_profile=0x7f080004;
-        public static final int lv_expiriences=0x7f080006;
-        public static final int txt_description=0x7f080003;
-        public static final int txt_title=0x7f080002;
-    }
-    public static final class layout {
-        public static final int activity_experiencias=0x7f030000;
-        public static final int activity_timeline=0x7f030001;
-        public static final int experiencias_custom_list_layout=0x7f030002;
-        public static final int fragment_experiencias=0x7f030003;
-    }
-    public static final class menu {
-        public static final int experiencias=0x7f070000;
-        public static final int timeline=0x7f070001;
-    }
-    public static final class string {
-        public static final int action_settings=0x7f050002;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050001;
-        public static final int title_activity_experiencias=0x7f050003;
->>>>>>> oscar
     }
     public static final class style {
         /** 
